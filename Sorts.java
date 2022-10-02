2b239a19d66c0cccf16091da57347c2f9ae145df
I <henrikl@tuta.io>
2022-10-02 19:18:29 +0200

    unknown

---
+++ Sorts.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class Sorts {
+    @Test
+    void doesSortCorrectly(){
+
+    }
+
+    void mergeSort(){
+
+    }
+
+    void quickSort(){
+        
+    }
+}
