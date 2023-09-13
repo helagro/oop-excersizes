01ea31190e45ad33a3128f7369100dac64963a19
Henrik L <helagro@outlook.com>
2023-09-13 10:38:04 +0200

    unspecified

--- smalltests/objectHasItself.js
+++ smalltests/objectHasItself.js
@@ -6,10 +6,32 @@ function ResCode(number){
 }
 
 const SUCCESS = new ResCode(0)
+const ERROR = new ResCode(1)
 
 const result = {
-    resCode: SUCCESS,
+    resCode: ERROR,
     message: 'success'
 }
 
+
+
+
 console.log(SUCCESS.resCode, result.resCode, SUCCESS.resCode === result.resCode) // true
+
+switch (result.resCode) {
+    case SUCCESS.resCode:
+        console.log('success from result')
+        break
+    case ERROR.resCode:
+        console.log('error from result')
+        break
+}
+
+switch (ERROR.resCode) {
+    case SUCCESS.resCode:
+        console.log('success from SUCCESS')
+        break
+    case ERROR.resCode:
+        console.log('error from SUCCESS')
+        break
+}
