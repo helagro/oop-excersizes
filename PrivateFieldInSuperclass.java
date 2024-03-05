6a3a7cd385faf6d7652aff9b20b668221d0043ca
Henrik L <helagro@outlook.com>
2024-03-05 10:41:54 +0100

    unspecified

---
+++ PrivateFieldInSuperclass.java
@@ -0,0 +1,9 @@
+
+
+// class SubType extends PrivateFieldInSuperclass {
+//     public static void main(String[] args) {
+//         System.out.println(johnCena);
+//         System.out.println(jonathanSeenA);
+//         System.out.println(joshSenap);
+//     }
+// }
--- smalltests/FinalAbstract.java
+++
@@ -1,7 +0,0 @@
-package smallTests;
-// package smalltests;
-// public final abstract class FinalAbstract {
-//     public static void main(String[] args) {
-//         System.out.println("I won't print");
-//     }
-// }
